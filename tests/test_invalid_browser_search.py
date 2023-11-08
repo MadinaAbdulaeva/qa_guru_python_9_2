c5465352a6030340f91a6b5ffd1f158210827eb6
elearner <abdulaeva.mad@yandex.ru>
2023-11-08 22:51:02 +0300

    Тесты на поиск в Google

--- tests/test_invalid_browser_search.py
+++ tests/test_invalid_browser_search.py
@@ -1,7 +1,7 @@
 from selene.support.shared import browser
 from selene import be, have
 
-def test_google_should_not_find_selene():
+def test_browser_invalid_search():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('**&kfjejfwlkfn').press_enter()
-    browser.element('#botstuff').should(have.text('По запросу **&kfjejfwlkfn ничего не найдено.'))
+    browser.element('[style="padding-top:.33em"]').should(have.text('По запросу **&kfjejfwlkfn ничего не найдено.'))
