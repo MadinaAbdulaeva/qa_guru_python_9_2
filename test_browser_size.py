b01900a2df1992eb99fe2df084798fcfad000cb4
elearner <abdulaeva.mad@yandex.ru>
2023-11-09 20:59:00 +0300

    перенос .py файлов из venv обратно в корень

---
+++ test_browser_size.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture()
+def chrome():
+    browser.open('https://google.com')
+    yield
+    browser.quit()
+
+@pytest.fixture()
+def open_browser(chrome):
+    pass
+
+@pytest.fixture
+def browser_size():
+    webdriver.set_window_size(1024, 768)
+
+def test_browser_size(open_browser, browser_size):
+    pass
