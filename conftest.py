d2edcdc7c9ce24e35f84e44f6cf63d32e6f8bf69
elearner <abdulaeva.mad@yandex.ru>
2023-11-10 23:13:34 +0300

    Доработка фикстуры и requrements.txt

--- conftest.py
+++ conftest.py
@@ -1,11 +1,7 @@
 import pytest
-from selenium import browser
+from selene import browser
 
 @pytest.fixture
 def browser_size():
     browser.config.window_width = 1024
     browser.config.window_height = 768
-    browser.config.browser_name = 'Chrome'
-
-    yield
-    browser.quit()
--- test_valid_browser_search.py
+++ test_valid_browser_search.py
@@ -1,8 +1,14 @@
 
 from selene.support.shared import browser
 from selene import be, have
+@pytest.fixture(scope='session', autouse=True) #фикстура на применение ко всем тестам
 
-def test_browser_valid_search():
+def test_browser_valid_search(): #успешный поиск
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_browser_invalid_search(): #поиск без результата
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('**&kfjejfwlkfn').press_enter()
+    browser.element('[style="padding-top:.33em"]').should(have.text('По запросу **&kfjejfwlkfn ничего не найдено.'))
